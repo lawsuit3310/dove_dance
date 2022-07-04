75486232ad16020a3d0465f90f88d0a5a333b3e1
lawsuit3310 <aimeet5480@gmail.com>
2022-07-04 21:14:29 +0900

    기본 캐릭터 스프라이트 및 애니메이션 추가

--- Assets/2.Scripts/Player.cs
+++ Assets/2.Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     GameManager gameManager;
     Rigidbody2D rigidbody2D;
 
+
     bool isJumpable = true;
     // Start is called before the first frame update
 
@@ -18,6 +19,7 @@ public class Player : MonoBehaviour
         gameManager = FindObjectOfType<GameManager>();
         getPlayerReference(gameManager.Character);
         rigidbody2D = GetComponent<Rigidbody2D>();
+
     }
     void Start()
     {
@@ -27,9 +29,9 @@ public class Player : MonoBehaviour
     void Update()
     {
         //실제 조작
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && rigidbody2D.velocity.y <= 0)
         {
-            this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x > 0 ? maxSpd : -maxSpd, 0);
+            this.rigidbody2D.velocity = new Vector2(this.rigidbody2D.velocity.x > 0 ? maxSpd : -maxSpd, -0.5f);
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
@@ -38,23 +40,24 @@ public class Player : MonoBehaviour
         else if (Input.GetKey(KeyCode.A))
         {
             this.rigidbody2D.velocity -= Vector2.right;
+            playerFilp(false);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             rigidbody2D.velocity += Vector2.right;
+            playerFilp(true);
         }
 
         //플레이어의 이동 속도 등을 제한 하는 부분 
         if (Mathf.Abs(rigidbody2D.velocity.x) > maxSpd)
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x > 0 ? maxSpd : -1 * maxSpd, rigidbody2D.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.W) && isJumpable)
+        if (Input.GetKey(KeyCode.W) && isJumpable)
         {
             rigidbody2D.velocity += Vector2.up * JumpScale;
         }
 
         isJumpable = this.rigidbody2D.velocity.y == 0 ? true : false;
-        Debug.Log(rigidbody2D.velocity.y);
     }
 
     
@@ -63,5 +66,18 @@ public class Player : MonoBehaviour
     {
         int Character = gameManager.Character;
         Debug.Log(Character);
+    }
+
+    void playerFilp(bool status)
+    {
+        //true면 뒤집고 false면 안뒤집기
+        if (status)
+        {
+            this.transform.localScale = new Vector3(-1, 1, this.transform.localScale.z);
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(1, 1, this.transform.localScale.z);
+        }
     }
 }
